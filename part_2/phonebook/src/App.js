351c6be6d05d8b76bfac9fd805e2d4e43d8cac75
riikkayoki <74421508+riikkayoki@users.noreply.github.com>
2022-09-07 12:59:44 +0300

    phonebook 2.7

--- part_2/phonebook/src/App.js
+++ part_2/phonebook/src/App.js
@@ -1,18 +1,28 @@
 import { useState } from 'react'
 
-const App = (props) => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ])
+
+const App = () => {
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
-    const nameObject =
-    {name: newName
+    const nameObject = {name: newName}
+    console.log(nameObject.name, 'uusi nimi')
+    console.log(persons, ' lista nimistä')
+    console.log(persons.includes(person => person.name === newName), 'onko nimi listassa')
+
+
+    if (persons.some(person => person.name === newName)) {
+      console.log(persons.includes(newName))
+
+        alert(`${newName} is already added to phonebook`)
+      }
+    else {
+      setPersons(persons.concat(nameObject))
+      setNewName('')
     }
-    setPersons(persons.concat(nameObject))
-    setNewName('')
+    return
   }
 
   const handleNameChange = (event) => {
