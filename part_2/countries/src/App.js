142686896cd39d5cf2c9c125e91e5da1f53ef12e
riikkayoki <74421508+riikkayoki@users.noreply.github.com>
2022-09-09 13:00:45 +0300

    countries 2.14

--- part_2/countries/src/App.js
+++ part_2/countries/src/App.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
+
 const Filter = ({newFilter, handleFilterChange}) => {
   return (
   <div>find countries:
@@ -10,7 +11,23 @@ const Filter = ({newFilter, handleFilterChange}) => {
   )
 }
 
+
 const Country = ({country}) => {
+  const api_key = process.env.REACT_APP_API_KEY
+  const [weather, setWeather] = useState([])
+  const [wind, setWind] = useState([])
+  const [picture, setPicture] = useState([])
+
+  useEffect(() => {
+    axios
+        .get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${api_key}&units=metric`)
+        .then(response => {
+            setWeather(response.data.main)
+            setWind(response.data.wind)
+            setPicture(response.data.weather[0])
+        })
+    }, [])
+
   return (
   <div>
   <h1>{country.name.common}</h1>
@@ -23,6 +40,13 @@ const Country = ({country}) => {
   {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
   </ul>
   <img src={country.flags.png} alt='flag' height='50' width='100'></img>
+  <h2>Weather in {country.capital}</h2>
+  <div><b>temperature: </b>
+  {weather.temp} Celcius</div>
+  <img></img>
+  <img src={`http://openweathermap.org/img/wn/${picture.icon}@2x.png`} alt='weather'></img>
+  <div><b>wind: </b>{wind.speed} m/s</div>
+
   </div>
   )
 }
@@ -40,7 +64,7 @@ const Countries = ({countriesToShow, handleClick}) => {
     }
 
     {countriesToShow.length === 1 &&
-      countriesToShow.map(country =>
+      countriesToShow.map(country => 
       <div key={country.name.common}><Country country={country}/></div>)
     }
     </div>
@@ -52,14 +76,16 @@ const App = () => {
   const [countries, setCountries] = useState([])
   const [showAll, setShowAll] = useState([])
 
+
+
   useEffect(() => {
     axios
       .get('https://restcountries.com/v3.1/all')
       .then(response => {
         setShowAll(response.data)
-
       })
   }, [])
+
 
   const handleFilterChange = (event) => {
     setCountries(showAll.filter(country => country.name.common.toLowerCase().includes(newFilter.toLowerCase())))
