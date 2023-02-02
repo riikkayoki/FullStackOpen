f8442325c2b297cc641bf9110a93496eebb94d88
riikkayoki <74421508+riikkayoki@users.noreply.github.com>
2023-02-02 10:46:07 +0200

    3.9

--- part_2/phonebook/src/services/personService.js
+++ part_2/phonebook/src/services/personService.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'http://localhost:3001/api/persons'
 
 const getAll = async () => {
     const request = axios.get(baseUrl)
-    const response = await request
-    return response.data
+    return request.then(response => response.data)
 }
 
 const create = async newObject => {
