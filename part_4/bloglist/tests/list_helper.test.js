186b53493b3de4ec91c15b4fbc63494204839b72
riikkayoki <74421508+riikkayoki@users.noreply.github.com>
2023-03-14 11:17:10 +0200

    4.4

---
+++ part_4/bloglist/tests/list_helper.test.js
@@ -0,0 +1,24 @@
+const listHelper = require('../utils/list_helper')
+const {noBlog, oneBlog, manyBlogs} = require('../utils/blog_helper')
+
+describe('dummy', () => {
+  test('dummy returns one', () => {
+    const result = listHelper.dummy(noBlog)
+    expect(result).toBe(1)
+  })
+})
+
+describe('total likes', () => {
+  test('totalLikes returns 0 when there are no blogs', () => {
+    const result = listHelper.totalLikes(noBlog)
+    expect(result).toBe(0)
+  }),
+  test('totalLikes returns the number of likes of a single blog', () => {
+    const result = listHelper.totalLikes(oneBlog)
+    expect(result).toBe(7)
+  }),
+  test('totalLikes returns the number of likes of many blogs', () => {
+    const result = listHelper.totalLikes(manyBlogs)
+    expect(result).toBe(36)
+  })
+})
