23055d0f4fc76cc7c28ae3dbee26cb90b7d59ddc
riikkasalojoki <riikka.salojoki@wolt.com>
2024-08-30 12:43:26 +0300

    7.1

--- part_7/routed-anecdotes/src/App.jsx
+++ part_7/routed-anecdotes/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import {
+  Routes, Route, Link
+} from 'react-router-dom'
 
 const Menu = () => {
   const padding = {
@@ -6,9 +9,9 @@ const Menu = () => {
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Link style={padding} to="/">anecdotes</Link>
+      <Link style={padding} to="create-new">create new</Link>
+      <Link style={padding} to="/about">about</Link>
     </div>
   )
 }
@@ -126,10 +129,12 @@ const App = () => {
     <div>
       <h1>Software anecdotes</h1>
       <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
+        <Route path="/create-new" element={<CreateNew addNew={addNew} />} />
+        <Route path="/about" element={<About />} />
+     </Routes>
+     <Footer/>
     </div>
   )
 }
