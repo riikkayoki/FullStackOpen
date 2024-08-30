4183519999f16694c33d5bec76027955f1586174
riikkasalojoki <riikka.salojoki@wolt.com>
2024-08-30 11:30:40 +0300

    remove part7 to do it all again with fresh memory

--- part_7/bloglist/bloglist-frontend/src/hooks/useFetchBlogQuery.js
+++
@@ -1,9 +0,0 @@
-import { useQuery } from 'react-query';
-import { fetchBlogs } from '../services/blogs';
-
-export const useFetchBlogsQuery = (id) => {
-    return useQuery({
-        queryKey: ['blogs', id],
-        queryFn: () => fetchBlogs(id),
-    })
-}
