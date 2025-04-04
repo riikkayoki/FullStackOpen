3d402a7419442ee7f218549ca168ba4f4c9bb0a7
riikkayoki <74421508+riikkayoki@users.noreply.github.com>
2025-04-04 13:24:14 +0300

    add 7.4-7.6

--- part_7/routed-anecdotes/src/App.jsx
+++ part_7/routed-anecdotes/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import { Routes, Route, Link, useMatch } from 'react-router-dom'
+import { Routes, Route, Link, useMatch, useNavigate } from 'react-router-dom'
+import  { useField } from './hooks'
 
 const Menu = () => {
   const padding = {
@@ -49,44 +50,56 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const {'reset': resetContent, ...content}  = useField('text')
+  const {'reset': resetAuthor, ...author} = useField('text')
+  const {'reset': resetInfo, ...info} = useField('text')
+  const navigate = useNavigate(props)
 
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
+  const handleSubmit = (event) => {
+    event.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
+    navigate('/')
   }
+
+  const handleReset = (event) => {
+    event.preventDefault()
+    resetContent()
+    resetAuthor()
+    resetInfo()
+
+  }
+
 
   return (
     <div>
+
       <h2>create a new anecdote</h2>
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author}/>
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
+        <button onClick={event => handleReset(event)}>reset</button>
       </form>
     </div>
   )
 
 }
-
 const Anecdote = ({ anecdote }) => {
 
   return (
